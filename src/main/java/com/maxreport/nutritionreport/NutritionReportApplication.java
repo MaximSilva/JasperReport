058a98340b95cbd1896d34fce8c32e69605c3a03
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-09-18 16:10:24 +0300

    Nutrient Table
    Add Nutrient Table wit Sorting

--- src/main/java/com/maxreport/nutritionreport/NutritionReportApplication.java
+++ src/main/java/com/maxreport/nutritionreport/NutritionReportApplication.java
@@ -1,10 +1,14 @@
 package com.maxreport.nutritionreport;
 
+import com.maxreport.nutritionreport.model.Nutrition;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -13,11 +17,40 @@ public class NutritionReportApplication {
     public static void main(String[] args) throws JRException {
         SpringApplication.run(NutritionReportApplication.class, args);
         String filepath = "D:\\Education\\MyJasperReport\\NutritionReport\\src\\main\\resources\\templates\\nutritionreport.jrxml";
+
+        Nutrition protein = new Nutrition("Protein", 62, 83, "g");
+        Nutrition carbonydrates = new Nutrition("Carbonydrates", 180, 206, "g");
+        Nutrition fiber = new Nutrition("Fiber", 20, 38, "g");
+        Nutrition sugars = new Nutrition("Sugars", 68, 62, "g");
+        Nutrition fat = new Nutrition("Fat", 60, 55, "g");
+        Nutrition cholesterol = new Nutrition("Cholesterol", 84, 300, "mg");
+        Nutrition sodium = new Nutrition("Sodium", 2200, 2300, "mg");
+        Nutrition potassium = new Nutrition("Potassium", 2000, 3500, "mg");
+        Nutrition calcium = new Nutrition("Calcium", 62, 100, "%");
+        Nutrition iron = new Nutrition("Iron", 38, 100, "%");
+
+        List<Nutrition> nutritionList = new ArrayList<>();
+        nutritionList.add(protein);
+        nutritionList.add(carbonydrates);
+        nutritionList.add(fiber);
+        nutritionList.add(sugars);
+        nutritionList.add(fat);
+        nutritionList.add(cholesterol);
+        nutritionList.add(sodium);
+        nutritionList.add(potassium);
+        nutritionList.add(calcium);
+        nutritionList.add(iron);
+
+        JRBeanCollectionDataSource nutritionDataSource = new JRBeanCollectionDataSource(nutritionList);
+
+
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("firstName", "Max");
         parameters.put("lastName", "Spirkin");
         parameters.put("dob", "05/07/2002");
         parameters.put("age", 21);
+        parameters.put("nutritionDataSet", nutritionDataSource);
+
         JasperReport report = JasperCompileManager.compileReport(filepath);
         JasperPrint print = JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
         JasperExportManager.exportReportToPdfFile(print, "D:\\Education\\MyJasperReport\\NutritionReport\\src\\main\\resources\\static\\nutritionreport.pdf");
---
+++ src/main/java/com/maxreport/nutritionreport/model/Nutrition.java
@@ -0,0 +1,14 @@
+package com.maxreport.nutritionreport.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Nutrition {
+    private String nutritionName;
+    private int total;
+    private int goal;
+    private String metric;
+
+}
