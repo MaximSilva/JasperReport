da39efd0946db9ddf192455946c773bdd65bf1e4
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-09-18 13:55:32 +0300

    nutritionreport.jrxml
    Add file nutritionreport.jrxml

---
+++ src/main/java/com/maxreport/nutritionreport/NutritionReportApplication.java
@@ -0,0 +1,13 @@
+package com.maxreport.nutritionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritionReportApplication.class, args);
+    }
+
+}
