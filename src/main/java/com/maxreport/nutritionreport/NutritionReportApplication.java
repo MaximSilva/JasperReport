53bba1261f7bd1c62a976059050ff1656914a5ea
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-09-18 14:27:37 +0300

    Bind Java and JavaReports
    Edit and transfer file nutritionreport.jrxml on the package resourse\\templates

--- src/main/java/com/maxreport/nutritionreport/NutritionReportApplication.java
+++ src/main/java/com/maxreport/nutritionreport/NutritionReportApplication.java
@@ -3,11 +3,21 @@ package com.maxreport.nutritionreport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class NutritionReportApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NutritionReportApplication.class, args);
+        String filepath = "D:\\Education\\MyJasperReport\\NutritionReport\\src\\main\\resources\\templates\\nutritionreport.jrxml";
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("firstName", "Max");
+        parameters.put("lastName", "Spirkin");
+        parameters.put("dob", "05/07/2002");
+        parameters.put("age", 21);
+
     }
 
 }
